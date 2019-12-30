8e82ff36731a5e1979841bf378277e40d757a0f3
Zain <zainarshad45122@gmail.com>
2019-12-31 01:16:07 +0500

    First Commit

---
+++ src/views/LandingPage/Sections/ProductSection.js
@@ -0,0 +1,60 @@
+import React from "react";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+
+// @material-ui/icons
+import Chat from "@material-ui/icons/Web";
+import VerifiedUser from "@material-ui/icons/StayCurrentPortrait";
+import Fingerprint from "@material-ui/icons/DesktopMac";
+// core components
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import InfoArea from "components/InfoArea/InfoArea.js";
+
+import styles from "assets/jss/material-kit-react/views/landingPageSections/productStyle.js";
+
+const useStyles = makeStyles(styles);
+
+export default function ProductSection() {
+  const classes = useStyles();
+  return (
+    <div className={classes.section}>
+      <GridContainer justify="center">
+        <GridItem xs={12} sm={12} md={8}>
+          <h2 className={classes.title}>Our Top Services</h2>
+        </GridItem>
+      </GridContainer>
+      <div>
+        <GridContainer>
+          <GridItem xs={12} sm={12} md={4}>
+            <InfoArea
+              title="Web Development"
+              description="Build your dream web app from scratch with help from skilled freelance coders"
+              icon={Chat}
+              iconColor="info"
+              vertical
+            />
+          </GridItem>
+          <GridItem xs={12} sm={12} md={4}>
+            <InfoArea
+              title="App Development"
+              description="Be where your audience is with mobile app services for every business"
+              icon={VerifiedUser}
+              iconColor="success"
+              vertical
+            />
+          </GridItem>
+          <GridItem xs={12} sm={12} md={4}>
+            <InfoArea
+              title="Graphic Designing"
+              description="Want to say more with less? Brand recognition is just a custom logo design away"
+              icon={Fingerprint}
+              iconColor="danger"
+              vertical
+            />
+          </GridItem>
+        </GridContainer>
+      </div>
+    </div>
+  );
+}
